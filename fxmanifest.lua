20989f7b6b822bea46559f8fc6d69e0c57df4dbf
Heros <herosticworld@gmail.com>
2025-08-03 22:37:22 +0200

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,6 +14,7 @@ server_script 'sv_chat.lua'
 files {
   'dist/ui.html',
   'dist/index.css',
+  'dist/chat.js',
   'html/vendor/*.css',
   'html/vendor/fonts/*.woff2',
 }
@@ -21,10 +22,4 @@ files {
 fx_version 'adamant'
 games { 'rdr3', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
-
-dependencies {
-  'yarn',
-  'webpack'
-}
-
-webpack_config 'webpack.config.js'
+nui_callback_strict_mode 'true'
